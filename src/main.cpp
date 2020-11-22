6506c826b591036dd5d55aabc72a4cf23aeab0e8
Leo B <l.bispo@live.com>
2020-11-22 15:58:58 +0100

    schematic and PIN definition

--- src/main.cpp
+++ src/main.cpp
@@ -12,9 +12,12 @@ Distributed as-is; no warranty is given.
 #include "secrets.h"
 
 #include <Ticker.h>
-
+#include <Wire.h>
 #include <WiFi.h>
-#include <PubSubClient.h>
+
+#include "ArduinoHttpClient.h"
+#include "PubSubClient.h"
+#include "RTClib.h"
 
 // MQTT reconnection timeout in ms
 #define MQTT_RC_TIMEOUT 5000
@@ -34,15 +37,30 @@ const char* sub_topic = "chirp/water";
 double temp           = NAN;
 float  temp_setpoint  = NAN;
 
-#define C_SENSE         A4                //GPIO32 - IO32/ADC1_CH4
-#define PWM             A17               //GPIO27, RTC_GPIO17
-#define BUZZER          A18               //GPIO25, RTC_GPIO6
+#define C_SENSE         A5                //GPIO33 - IO33/ADC1_CH5
+#define PWM             A14               //GPIO13
+#define BUZZER          A15               //GPIO12
+#define SDA_PIN         A13               //GPIO15
+#define SCL_PIN         A10               //GPIO04
 
 #define SLEEP_TIME_S    (8 * 60 * 60)     //seconds
 
 // initialize the MQTT Client
 WiFiClient espClient;
 PubSubClient client(espClient);
+
+// HTTP clients
+HttpClient http_time(espClient, "worldclockapi.com");
+HttpClient http_mail(espClient, "emailserver.com");
+
+// RTC instance
+RTC_PCF8563 rtc;
+
+char iso8601date[21]      = "";
+
+// Sense if wake is from RTC or Button
+// May not need, as one can read the I2C and confirm, to check power requirements
+const uint8_t INT_PIN = 2; 
 
 // Timer instances
 Ticker mqtt_rc;
@@ -176,26 +194,89 @@ void read_moisture()
   }
 }
 
+void getInternetTime()
+{
+  // http://worldclockapi.com/api/json/utc/now
+  
+  http_time.beginRequest();
+
+  http_time.get("/api/json/cet/now");
+
+  // or get from home assistant
+  //String _bearer = "Bearer ";
+  //_bearer.concat( S_ha_token );
+  //http_time.sendHeader("Authorization", _bearer);
+
+  http_time.endRequest();
+
+  int statusCode = http_time.responseStatusCode();
+
+  Serial.printf("GET Status code: %d\n", statusCode);
+
+  if (statusCode != 200)
+  {
+    //PROBLEM
+    //sleep
+    return;
+  }
+  
+  http_time.skipResponseHeaders();
+
+  String response = http_time.responseBody();
+
+  int _index = response.indexOf("\"currentDateTime\": ");
+
+  // "currentDateTime": "2020-11-22T11:10Z",
+  response.substring(_index + 20, _index + 20 + 17);
+  // RTC lib expect seconds, so insert 00 to avoid trouble
+  response += ":00Z";
+
+  response.toCharArray(iso8601date, sizeof(iso8601date));  
+}
+
 void setup()
 {
   Serial.begin(115200);
 
   while (!Serial) delay(1); // wait for Serial on Leonardo/Zero, etc
 
-  Serial.println("MAX31855 test");
-
-  // wait for MAX chip to stabilize
-  delay(500);
-
-  Serial.print("Initializing sensor...");
-
-  // ...
-
-  Serial.println("DONE.");
+  Serial.println("RTC init");
+  
+  Wire.begin(SDA_PIN, SCL_PIN, 100000L);
+  if (! rtc.begin())
+  {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    abort();
+  }
 
   setup_wifi();
   client.setServer(mqtt_server, mqtt_port);
   client.setCallback(callback);
+
+  if (rtc.lostPower())
+  {
+    Serial.println("RTC is NOT initialized, let's set the time!");
+    // When time needs to be set on a new device, or after a power loss, the
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+    //
+    // Note: allow 2 seconds after inserting battery or applying external power
+    // without battery before calling adjust(). This gives the PCF8523's
+    // crystal oscillator time to stabilize. If you call adjust() very quickly
+    // after the RTC is powered, lostPower() may still return true.
+
+    rtc.adjust(DateTime(iso8601date));
+  }
+
+  // When the RTC was stopped and stays connected to the battery, it has
+  // to be restarted by clearing the STOP bit. Let's do this to ensure
+  // the RTC is running.
+  rtc.start();
+
 }
 
 void loop()
